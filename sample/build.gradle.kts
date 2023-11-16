5aee9b42a13cb825d3fbcae97afa4096005d4b4f
matejsemancik <matej.semancik@gmail.com>
2023-11-16 20:33:26 +0100

    Update README

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -27,21 +27,44 @@ plugins {
 //}
 
 // Muzeum
+//sheetHappens {
+//    spreadsheetId = "1eq7TTBbJ1PyMfiXYIXKm77o4Zvpus_oErgOF3bH0-A4"
+//    sheetName = "Translations_draft"
+//    apiKey = localProperties.getProperty("GOOGLE_API_KEY")
+//
+//    sheetLayout {
+//        keyColumnName = "key_android"
+//        sectionColumnName = "Section"
+//        languageColumn("EN" to "values")
+//        languageColumn("CZ" to "values-cs")
+//        languageColumn("CZ" to "values-sk")
+//        languageColumn("UK" to "values-uk")
+//    }
+//
+//    resourcesLayout {
+//        resourcesDir = layout.projectDirectory.dir("src/main/res")
+//    }
+//}
+
 sheetHappens {
-    spreadsheetId = "1eq7TTBbJ1PyMfiXYIXKm77o4Zvpus_oErgOF3bH0-A4"
-    sheetName = "Translations_draft"
-    apiKey = localProperties.getProperty("GOOGLE_API_KEY")
+    spreadsheetId.set("Spreadsheet ID, eg. bKGtVRjP-m_HNsiZJNE5qWH3FweSNlRQv4tsM1WkF65J7ZgqB_WWqN")
+    sheetName.set("Sheet name, eg. Sheet1")
+    apiKey.set("API Key ****")
 
     sheetLayout {
-        keyColumnName = "key_android"
-        sectionColumnName = "Section"
+        sectionColumnName.set("section") // Optional
+        keyColumnName.set("key")
+
+        // Add language column for each translation
         languageColumn("EN" to "values")
-        languageColumn("CZ" to "values-cs")
-        languageColumn("CZ" to "values-sk")
-        languageColumn("UK" to "values-uk")
+        languageColumn("SK" to "values-sk")
     }
 
     resourcesLayout {
-        resourcesDir = layout.projectDirectory.dir("src/main/res")
+        resourcesDir.set(layout.projectDirectory.dir("src/main/res"))
+
+        splitResources = true // Optional, default `false`
+        stringsFileName = "strings.xml" // Optional, default "strings.xml"
+        pluralsFileName = "plurals.xml" // Optional, default "plurals.xml"
     }
 }
